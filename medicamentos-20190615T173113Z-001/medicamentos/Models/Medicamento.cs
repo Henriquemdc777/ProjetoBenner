a3182d4677c8f7ed939bc73b76145dc0d51a93c2
Henrique Medeiros da Costa <rikmdc777@gmail.com>
2019-06-15 15:08:59 -0300

    1

---
+++ medicamentos-20190615T173113Z-001/medicamentos/Models/Medicamento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace medicamentos.Models
+{
+    public class Medicamento
+    {
+        public string Medicamentoid { get; set; }
+        public string Nome { get; set; }
+        public string Composicao { get; set; }
+        public string Unidade { get; set; }
+    }
+}
---
+++ medicamentos-20190615T173113Z-001/medicamentos/Models/medicacao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace medicamentos.Models
+{
+    public class Medicacao
+    {
+        public int Medicacaoid { get; set; }
+        public int Medicamentoid { get; set; }
+        public int Intervalo { get; set; }
+        public string Dose { get; set; }
+        public string DoseTotal { get; set; }
+    }
+}
