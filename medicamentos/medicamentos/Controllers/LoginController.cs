5545f7671b27650bfe98de4fa6f25df65b53a5ea
Henrique Medeiros da Costa <rikmdc777@gmail.com>
2019-07-18 22:18:46 -0300

    10

--- medicamentos/medicamentos/Controllers/LoginController.cs
+++ medicamentos/medicamentos/Controllers/LoginController.cs
@@ -16,40 +16,72 @@ namespace medicamentos.Controllers
         {
             return View();
         }
-        public ActionResult Cadastro(String Nome, String CPF, String senha, DateTime datanasc)
+        public ActionResult Cadastro()
         {
+            return View();
+        }
+        public ActionResult CadastroUsuario(String Nome, String CPF, String senha, DateTime datanasc)
+        {
+            MedicamentosContext contexto = new MedicamentosContext();
             UsuarioDAO dao = new UsuarioDAO();
             Usuario usuario = new Usuario();
             usuario.Cpf = CPF;
             usuario.Senha = senha;
             usuario.Nome = Nome;
-            usuario.Datanasc = datanasc;
+            usuario.Datanasc = datanasc.Date;
             dao.Adicionar(usuario);
+            contexto.SaveChanges();
+            return RedirectToAction("Login");
+        }
+        public ActionResult CadastroMedico(String Nome, String CPF, String senha, DateTime datanasc, int CRM, string Desc, string Esp)
+        {
+            MedicamentosContext contexto = new MedicamentosContext();
+            MedicoDAO dao = new MedicoDAO();
+            Medico medico = new Medico();
+            medico.Cpf = CPF;
+            medico.Senha = senha;
+            medico.Nome = Nome;
+            medico.Datanasc = datanasc;
+            medico.Crm = CRM;
+            medico.Descricao = Desc;
+            medico.Especialidade = Esp;
+            dao.Adicionar(medico);
+            contexto.SaveChanges();
             return RedirectToAction("CadastroEndereco");
         }
-        public ActionResult CadastroEndereco (String CPF, String senha)
+        public ActionResult CadastroSecretaria(String Nome, String CPF, String senha, DateTime datanasc)
         {
-            return RedirectToAction("Index");
+            MedicamentosContext contexto = new MedicamentosContext();
+            SecretariaDAO dao = new SecretariaDAO();
+            Secretaria secretaria = new Secretaria();
+            secretaria.Cpf = CPF;
+            secretaria.Senha = senha;
+            secretaria.Nome = Nome;
+            secretaria.Datanasc = datanasc;
+            dao.Adicionar(secretaria);
+            contexto.SaveChanges();
+            return RedirectToAction("CadastroEndereco");
         }
+        
         public ActionResult Autentica(String CPF, String senha)
         {
             UsuarioDAO dao = new UsuarioDAO();
-            MedicoDAO dao2 = new MedicoDAO();
-            SecretariaDAO dao3 = new SecretariaDAO();
             Usuario usuario = dao.Busca(CPF, senha);
-            Medico medico = dao2.Busca(CPF, senha);
-            Secretaria secretaria = dao3.Busca(CPF, senha);
             if (usuario != null)
             {
                 Session["usuarioLogado"] = usuario;
                 return RedirectToAction("Usuario", "Home");
             }
-            else if(medico != null)
+            MedicoDAO dao2 = new MedicoDAO();
+            Medico medico = dao2.Busca(CPF, senha);
+            if (medico != null)
             {
                 Session["usuarioLogado"] = medico;
                 return RedirectToAction("Medico", "Home");
             }
-            else if (secretaria != null)
+            SecretariaDAO dao3 = new SecretariaDAO();
+            Secretaria secretaria = dao3.Busca(CPF, senha);
+            if (secretaria != null)
             {
                 Session["usuarioLogado"] = secretaria;
                 return RedirectToAction("Secretaria", "Home");
--- medicamentos/medicamentos/Models/Medico.cs
+++ medicamentos/medicamentos/Models/Medico.cs
@@ -10,7 +10,7 @@ namespace medicamentos.Models
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Cpf { get; set; }
-        public int CRM { get; set; }
+        public int Crm { get; set; }
         public DateTime Datanasc { get; set; }
         public string Senha { get; set; }
         public string Descricao { get; set; }
